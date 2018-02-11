e68fb9e117252971452c1c979374411f8f17d079
Lenny Lofberg <lenny@Lennys-MacBook-Pro-5779.local>
2018-02-10 17:00:33 -0800

    Adds AvroConverter example

--- bin/debug.sh
+++ bin/debug.sh
@@ -8,4 +8,6 @@ set -e
 export KAFKA_JMX_OPTS="-Xdebug -agentlib:jdwp=transport=dt_socket,server=y,suspend=${SUSPEND},address=5005"
 export CLASSPATH="$(find target/kafka-connect-aws-lambda-1.0-SNAPSHOT-package/share/java -type f -name '*.jar' | tr '\n' ':')"
 
-connect-standalone config/connect-json-docker.properties config/AwsLambdaSinkConnector.properties
+# connect-standalone config/connect-json-docker.properties config/AwsLambdaSinkConnector.properties
+
+connect-standalone config/connect-avro-docker.properties config/AwsLambdaSinkConnector.properties
