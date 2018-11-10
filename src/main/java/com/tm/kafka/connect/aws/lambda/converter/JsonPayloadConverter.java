fd5f1fb6aa5a50ba30f3d9d35243bfc183fa0166
lenny <lenny@lofberg.com>
2018-11-10 10:23:27 -0800

    Update dependency versions

--- src/main/java/com/tm/kafka/connect/aws/lambda/converter/JsonPayloadConverter.java
+++ src/main/java/com/tm/kafka/connect/aws/lambda/converter/JsonPayloadConverter.java
@@ -1,7 +1,6 @@
 package com.tm.kafka.connect.aws.lambda.converter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.json.JsonConverter;
@@ -10,27 +9,31 @@ import org.apache.kafka.connect.sink.SinkRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
+import static java.util.Collections.emptyMap;
 
 public class JsonPayloadConverter implements SinkRecordToPayloadConverter {
   private Logger log = LoggerFactory.getLogger(JsonPayloadConverter.class);
   private ObjectMapper objectMapper = new ObjectMapper();
+  private JsonConverter jsonConverter = new JsonConverter();
+  private JsonDeserializer jsonDeserializer = new JsonDeserializer();
+
+  public JsonPayloadConverter() {
+    jsonConverter.configure(emptyMap(), false);
+    jsonDeserializer.configure(emptyMap(), false);
+  }
 
   public String convert(SinkRecord record) throws JsonProcessingException {
-    Object value = record.value();
-    Schema schema = record.valueSchema();
     String topic = record.topic();
+    Schema schema = record.valueSchema();
+    Object value = record.value();
 
-    JsonConverter jsonConverter = new JsonConverter();
-    jsonConverter.configure(new HashMap<>(), false);
-    byte[] a = jsonConverter.fromConnectData(topic, schema, value);
-    JsonDeserializer jsonDeserializer = new JsonDeserializer();
-    jsonDeserializer.configure(new HashMap<>(), false);
-    JsonNode b = jsonDeserializer.deserialize(record.topic(), a);
+    String payload = objectMapper.writeValueAsString(
+      jsonDeserializer.deserialize(topic,
+        jsonConverter.fromConnectData(topic, schema, value)));
 
-    String payload = objectMapper.writeValueAsString(b);
-
-    log.trace("P: {}", payload);
+    if (log.isTraceEnabled()) {
+      log.trace("P: {}", payload);
+    }
 
     return payload;
   }
