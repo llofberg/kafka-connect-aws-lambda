aa99d1693b57b867f0394e7ef0ad0b158c67fbbd
Jordan Moore <crikket.007@gmail.com>
2019-04-16 15:53:51 -0500

    Fix debug script
    Built package no longer uses SNAPSHOT

--- bin/debug.sh
+++ bin/debug.sh
@@ -6,7 +6,7 @@
 set -e
 
 export KAFKA_JMX_OPTS="-Xdebug -agentlib:jdwp=transport=dt_socket,server=y,suspend=${SUSPEND},address=5005"
-export CLASSPATH="$(find target/kafka-connect-aws-lambda-1.0-SNAPSHOT-package/share/java -type f -name '*.jar' | tr '\n' ':')"
+export CLASSPATH="$(find target/kafka-connect-aws-lambda-1.0-package/share/java -type f -name '*.jar' | tr '\n' ':')"
 
 # connect-standalone config/connect-json-docker.properties config/AwsLambdaSinkConnector.properties
 
